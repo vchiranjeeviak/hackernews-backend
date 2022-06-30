3e50baefaa6c7f0f9e6b4aa186e25322f81793be
Chiranjeevi <vchiranjeeviak.tirunagari@gmail.com>
2022-06-30 18:32:22 +0530

    Extended query to get feed containing all links

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,4 +1,4 @@
-import { objectType } from "nexus";
+import { extendType, objectType } from "nexus";
 
 export const Link = objectType({
     name: "Link",
@@ -6,5 +6,17 @@ export const Link = objectType({
         t.nonNull.int("id");
         t.nonNull.string("url");
         t.nonNull.string("description");
+    },
+})
+
+export const LinkQuery = extendType({
+    type: "Query",
+    definition(t) {
+        t.nonNull.list.nonNull.field("feed", {
+            type: "Link",
+            async resolve(parent, args, context, info) {
+                return await context.prisma.link.findMany();
+            }
+        })
     },
 })
