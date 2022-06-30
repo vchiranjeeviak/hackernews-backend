4c964fb718dc3159f674249b14b11ce04e73ac8d
Chiranjeevi <vchiranjeeviak.tirunagari@gmail.com>
2022-06-30 18:51:19 +0530

    created context and place prisma client object in it and passing it in server initialisation

--- nexus-typegen.ts
+++ nexus-typegen.ts
@@ -4,7 +4,7 @@
  */
 
 
-
+import type { Context } from "./src/context"
 
 
 
@@ -102,7 +102,7 @@ export type NexusGenFeaturesConfig = {
 }
 
 export interface NexusGenTypes {
-  context: any;
+  context: Context;
   inputTypes: NexusGenInputs;
   rootTypes: NexusGenRootTypes;
   inputTypeShapes: NexusGenInputs & NexusGenEnums & NexusGenScalars;
