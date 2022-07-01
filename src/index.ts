eed2bf817b5604a214072cc64c4112cf9594f360
Chiranjeevi Tirunagari <vchiranjeeviak.tirunagari@gmail.com>
2022-07-01 15:27:52 +0530

    Configured postgres db from docker image

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 
 import { schema } from "./schema";
 import { context } from "./context";
 
 export const server = new ApolloServer({
     schema,
-    context
+    context,
+    introspection: true,
+    plugins: [ApolloServerPluginLandingPageLocalDefault()]
 });
 
 const port = process.env.PORT || 3000;
